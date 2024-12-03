f4e9514274c7a03b695ebfdc80d1443f8212e701
Mateo Sebastian Sotomayor Benavides <mateo.sotomayor@udla.edu.ec>
2024-12-03 07:19:26 -0500

    Agregar archivos de proyecto.

---
+++ MateoSotomayorProgreso2/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MateoSotomayorProgreso2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
